17b3c709c96ed066c52c65e7b947ab677d72ab4e
Sudhanshu agrawal <46307444+Asudhanshu@users.noreply.github.com>
2020-04-10 23:58:47 +0530

    Update PythonCodeOfAlgorithm.py

--- PythonCodeOfAlgorithm.py
+++ PythonCodeOfAlgorithm.py
@@ -44,7 +44,7 @@ for i in range(0,len(l1)):
 print(l2)
 
 #Reading the training .csv file
-df=pd.read_csv("Prototype.csv")
+df=pd.read_csv("training.csv")
 
 #Replace the values in the imported file by pandas by the inbuilt function replace in pandas.
 
@@ -65,7 +65,7 @@ print(X)
 print(y)
 
 #Reading the  testing.csv file
-tr=pd.read_csv("prototype 1.csv")
+tr=pd.read_csv("testing.csv")
 
 #Using inbuilt function replace in pandas for replacing the values
 

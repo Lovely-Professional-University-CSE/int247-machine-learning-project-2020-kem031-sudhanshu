67dcb802d4e6e8addc1956481572b6f28952a5e3
Sudhanshu agrawal <46307444+Asudhanshu@users.noreply.github.com>
2020-04-11 20:14:47 +0530

    Add files via upload

---
+++ Database/database.py
@@ -0,0 +1,42 @@
+#These code is to be inserted in paericular algorithm function in PythonCodeOfAlgorithm.py
+
+#Creating table for storing the values in databse.db for Random Forest Algorithm
+import s3qlite3 
+conn = sqlite3.connect('database.db') 
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS RandomForest(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+c.execute("INSERT INTO RandomForest(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred2.get()))
+conn.commit()
+c.close() 
+conn.close()
+
+#Creating table for storing the values in databse.db for Decision Tree Algorithm
+import sqlite3 
+conn = sqlite3.connect('database.db') 
+c = conn.cursor() 
+c.execute("CREATE TABLE IF NOT EXISTS DecisionTree(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+c.execute("INSERT INTO DecisionTree(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred1.get()))
+conn.commit()  
+c.close() 
+conn.close()
+
+#Creating table for storing the values in databse.db for K-Nearest Neighbour Algorithm
+import sqlite3 
+conn = sqlite3.connect('database.db') 
+c = conn.cursor() 
+c.execute("CREATE TABLE IF NOT EXISTS KNearestNeighbour(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+c.execute("INSERT INTO KNearestNeighbour(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred4.get()))
+conn.commit()  
+c.close() 
+conn.close()
+
+#Creating table for storing the values in databse.db for Naive Bayes Algorithm
+import sqlite3 
+        conn = sqlite3.connect('database.db') 
+        c = conn.cursor() 
+        c.execute("CREATE TABLE IF NOT EXISTS NaiveBayes(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+        c.execute("INSERT INTO NaiveBayes(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred3.get()))
+        conn.commit()  
+        c.close() 
+        conn.close()
+        

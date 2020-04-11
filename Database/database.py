a9d8548555320a9fa9ba88debd2fc7b76a539a7b
Rohan agrawal <42538289+agrawalrohan52@users.noreply.github.com>
2020-04-12 01:55:59 +0530

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -32,11 +32,11 @@ conn.close()
 
 #Creating table for storing the values in databse.db for Naive Bayes Algorithm
 import sqlite3 
-        conn = sqlite3.connect('database.db') 
-        c = conn.cursor() 
-        c.execute("CREATE TABLE IF NOT EXISTS NaiveBayes(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
-        c.execute("INSERT INTO NaiveBayes(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred3.get()))
-        conn.commit()  
-        c.close() 
-        conn.close()
+conn = sqlite3.connect('database.db') 
+c = conn.cursor() 
+c.execute("CREATE TABLE IF NOT EXISTS NaiveBayes(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
+c.execute("INSERT INTO NaiveBayes(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred3.get()))
+conn.commit()  
+c.close() 
+conn.close()
         

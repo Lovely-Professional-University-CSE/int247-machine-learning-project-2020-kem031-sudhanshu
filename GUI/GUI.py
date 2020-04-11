2432ba6e1099c91cbff587ba6be227518b063a6e
Sudhanshu agrawal <46307444+Asudhanshu@users.noreply.github.com>
2020-04-11 20:22:31 +0530

    Add files via upload

---
+++ GUI/GUI.py
@@ -0,0 +1,170 @@
+#Tk class is used to create a root window
+root = Tk()
+root.configure(background='Ivory')
+root.title('Smart Disease Predictor System')
+root.resizable(0,0)
+
+#taking first input as symptom
+Symptom1 = StringVar()
+Symptom1.set("Select Here")
+
+#taking second input as symptom
+Symptom2 = StringVar()
+Symptom2.set("Select Here")
+
+#taking third input as symptom
+Symptom3 = StringVar()
+Symptom3.set("Select Here")
+
+#taking fourth input as symptom
+Symptom4 = StringVar()
+Symptom4.set("Select Here")
+
+#taking fifth input as symptom
+Symptom5 = StringVar()
+Symptom5.set("Select Here")
+Name = StringVar()
+
+#function to Reset the given inputs to initial position
+prev_win=None
+def Reset():
+    global prev_win
+
+    Symptom1.set("Select Here")
+    Symptom2.set("Select Here")
+    Symptom3.set("Select Here")
+    Symptom4.set("Select Here")
+    Symptom5.set("Select Here")
+    
+    NameEn.delete(first=0,last=100)
+    
+    pred1.set(" ")
+    pred2.set(" ")
+    pred3.set(" ")
+    pred4.set(" ")
+    try:
+        prev_win.destroy()
+        prev_win=None
+    except AttributeError:
+        pass
+    
+#Exit button to come out of system
+from tkinter import messagebox
+def Exit():
+    qExit=messagebox.askyesno("System","Do you want to exit the system")
+    if qExit:
+        root.destroy()
+        exit()
+        
+#Headings for the GUI written at the top of GUI
+w2 = Label(root, justify=LEFT, text="Disease Predictor using Machine Learning", fg="Red", bg="Ivory")
+w2.config(font=("Times",30,"bold italic"))
+w2.grid(row=1, column=0, columnspan=2, padx=100)
+w2 = Label(root, justify=LEFT, text="Contributors: Sudhanshu,Rohan,Aditya", fg="Pink", bg="Ivory")
+w2.config(font=("Times",30,"bold italic"))
+w2.grid(row=2, column=0, columnspan=2, padx=100)
+
+#Label for the name
+NameLb = Label(root, text="Name of the Patient", fg="Red", bg="Ivory")
+NameLb.config(font=("Times",15,"bold italic"))
+NameLb.grid(row=6, column=0, pady=15, sticky=W)
+
+#Creating Labels for the symtoms
+S1Lb = Label(root, text="Symptom 1", fg="Black", bg="Ivory")
+S1Lb.config(font=("Times",15,"bold italic"))
+S1Lb.grid(row=7, column=0, pady=10, sticky=W)
+
+S2Lb = Label(root, text="Symptom 2", fg="Black", bg="Ivory")
+S2Lb.config(font=("Times",15,"bold italic"))
+S2Lb.grid(row=8, column=0, pady=10, sticky=W)
+
+S3Lb = Label(root, text="Symptom 3", fg="Black",bg="Ivory")
+S3Lb.config(font=("Times",15,"bold italic"))
+S3Lb.grid(row=9, column=0, pady=10, sticky=W)
+
+S4Lb = Label(root, text="Symptom 4", fg="Black", bg="Ivory")
+S4Lb.config(font=("Times",15,"bold italic"))
+S4Lb.grid(row=10, column=0, pady=10, sticky=W)
+
+S5Lb = Label(root, text="Symptom 5", fg="Black", bg="Ivory")
+S5Lb.config(font=("Times",15,"bold italic"))
+S5Lb.grid(row=11, column=0, pady=10, sticky=W)
+
+#Labels for the different algorithms
+lrLb = Label(root, text="DecisionTree", fg="white", bg="red", width = 20)
+lrLb.config(font=("Times",15,"bold italic"))
+lrLb.grid(row=15, column=0, pady=10,sticky=W)
+
+destreeLb = Label(root, text="RandomForest", fg="Red", bg="Orange", width = 20)
+destreeLb.config(font=("Times",15,"bold italic"))
+destreeLb.grid(row=17, column=0, pady=10, sticky=W)
+
+ranfLb = Label(root, text="NaiveBayes", fg="White", bg="green", width = 20)
+ranfLb.config(font=("Times",15,"bold italic"))
+ranfLb.grid(row=19, column=0, pady=10, sticky=W)
+
+knnLb = Label(root, text="kNearestNeighbour", fg="Red", bg="Sky Blue", width = 20)
+knnLb.config(font=("Times",15,"bold italic"))
+knnLb.grid(row=21, column=0, pady=10, sticky=W)
+OPTIONS = sorted(l1)
+
+#Taking name as input from user
+NameEn = Entry(root, textvariable=Name)
+NameEn.grid(row=6, column=1)
+
+#Taking Symptoms as input from the dropdown from the user
+S1 = OptionMenu(root, Symptom1,*OPTIONS)
+S1.grid(row=7, column=1)
+
+S2 = OptionMenu(root, Symptom2,*OPTIONS)
+S2.grid(row=8, column=1)
+
+S3 = OptionMenu(root, Symptom3,*OPTIONS)
+S3.grid(row=9, column=1)
+
+S4 = OptionMenu(root, Symptom4,*OPTIONS)
+S4.grid(row=10, column=1)
+
+S5 = OptionMenu(root, Symptom5,*OPTIONS)
+S5.grid(row=11, column=1)
+
+#Buttons for predicting the disease using different algorithms
+dst = Button(root, text="Prediction 1", command=DecisionTree,bg="Red",fg="yellow")
+dst.config(font=("Times",15,"bold italic"))
+dst.grid(row=6, column=3,padx=10)
+
+rnf = Button(root, text="Prediction 2", command=randomforest,bg="Light green",fg="red")
+rnf.config(font=("Times",15,"bold italic"))
+rnf.grid(row=7, column=3,padx=10)
+
+lr = Button(root, text="Prediction 3", command=NaiveBayes,bg="Blue",fg="white")
+lr.config(font=("Times",15,"bold italic"))
+lr.grid(row=8, column=3,padx=10)
+
+kn = Button(root, text="Prediction 4", command=KNN,bg="sky blue",fg="red")
+kn.config(font=("Times",15,"bold italic"))
+kn.grid(row=9, column=3,padx=10)
+
+rs = Button(root,text="Reset Inputs", command=Reset,bg="yellow",fg="purple",width=15)
+rs.config(font=("Times",15,"bold italic"))
+rs.grid(row=10,column=3,padx=10)
+
+ex = Button(root,text="Exit System", command=Exit,bg="yellow",fg="purple",width=15)
+ex.config(font=("Times",15,"bold italic"))
+ex.grid(row=11,column=3,padx=10)
+
+#Showing the output of different algorithms
+t1=Label(root,font=("Times",15,"bold italic"),text="Decision Tree",height=1,bg="Light green"
+         ,width=40,fg="red",textvariable=pred1,relief="sunken").grid(row=15, column=1, padx=10)
+
+t2=Label(root,font=("Times",15,"bold italic"),text="Random Forest",height=1,bg="Purple"
+         ,width=40,fg="white",textvariable=pred2,relief="sunken").grid(row=17, column=1, padx=10)
+
+t3=Label(root,font=("Times",15,"bold italic"),text="Naive Bayes",height=1,bg="red"
+         ,width=40,fg="orange",textvariable=pred3,relief="sunken").grid(row=19, column=1, padx=10)
+
+t4=Label(root,font=("Times",15,"bold italic"),text="kNearest Neighbour",height=1,bg="Blue"
+         ,width=40,fg="yellow",textvariable=pred4,relief="sunken").grid(row=21, column=1, padx=10)
+
+#calling this function because the application is ready to run
+root.mainloop()
